696fba2b3d6ace839292c2b5eca9d089379361ee
Edward Park <edwardpark@nyu.edu>
2019-01-29 16:37:21 -0500

    load bug fix

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
- $(document).ready(function() {
+$(document).ready(function() {
 
   console.log("JavaScript Loaded!");
-
-  if ($(window).width() > 450) {
-
-    jQuery('body').css('display','none');
-
-    jQuery(document).ready(function() {
-      jQuery('body').fadeIn();
-      jQuery('.logo').on('click',function(event) {
-        var thetarget = this.getAttribute('target')
-        if (thetarget != "_blank"){     
-          var thehref = this.getAttribute('href')
-          event.preventDefault();
-          jQuery('body').fadeOut(function() {
-            // alert(thehref)
-            window.location = thehref         
-          });
-        }
-      });
-    });
-
-    setTimeout(function() {
-      jQuery('body').fadeIn();
-    }, 1000)
-  }
    
   var heightOfWindow = $(window).scrollTop() + $(window).height();
   var halfOfWindow = heightOfWindow / 2;
+
+  $(document).on('scroll', function() {
+
+    if ($(".content").length > 0) {
+      var triggerDivTopPos = $(".content").offset().top - $(window).scrollTop();
+
+      if (triggerDivTopPos < halfOfWindow) {
+        $(".bg-img").css("opacity", "0.3");
+        $(".fade-out").css("opacity", "0");
+      } else {
+        $(".bg-img").css("opacity", "1");
+        $(".fade-out").css("opacity", "1");
+      }
+    }
+
+    if ($(".casestudy-wpr").length > 0) {
+      var caseStudyWprTop = $(".casestudy-wpr").offset().top - $(window).scrollTop();
+
+      if (caseStudyWprTop < halfOfWindow) {
+        $(".fade-out").css("opacity", "0");
+      } else {
+        $(".fade-out").css("opacity", "1");
+      }
+    }
+    
+  })
 
   // This notification will fade in after 2 seconds (2000 miliseconds is how JavaScript determines 2 seconds)
   setTimeout(function() {
